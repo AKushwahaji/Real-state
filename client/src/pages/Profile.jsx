9e29b731c009063a2d4f2ef216d5a66d1a616073
aniket kushwaha <aniketkushwaha13579@gmail.com>
2024-07-10 23:03:00 +0530

    delete listing feature created

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -34,11 +34,14 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState({});
+
   const dispatch = useDispatch();
 
 
 
-  console.log(formData);
+  console.log(userListings);
 
   useEffect(() => {
     if (file) {
@@ -137,6 +140,41 @@ const Profile = () => {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+  const handleListingDelete = async (listingId) => {
+
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(error.message);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => (listing._id !== listingId)));
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto ">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -191,15 +229,55 @@ const Profile = () => {
 
       </form>
 
-      <p className="text-red-700 mt-5">{error ? error : ''}</p>
-      <p className="text-green-700 mt-5">{updateSuccess ? "profile Updated successfully " : ''}</p>
+
 
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer"> Delete account</span>
         <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+
+      <p className="text-red-700 mt-5">{error ? error : ''}</p>
+      <p className="text-green-700 mt-5">{updateSuccess ? "profile Updated successfully " : ''}
+      </p>
+
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">{showListingsError ? "Error showing listings" : ""}</p>
+
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4 ">
+          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+              <Link to={`/listing/ ${listing._id}`}>
+                <img src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className=" h-20 w-16 object-contain" />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold hover:underline truncate flex"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col items-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase">
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 upppercase"> Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Profile
